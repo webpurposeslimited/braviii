b52ef6670d32b6a1d26a0c53b551e9a57eb783f9
WEB PURPOSES LIMITED <webpurposes@pm.me>
2026-02-08 15:03:33 +0500

    l

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,7 +37,9 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  const token = request.cookies.get('next-auth.session-token')?.value ||
+  const token = request.cookies.get('authjs.session-token')?.value ||
+    request.cookies.get('__Secure-authjs.session-token')?.value ||
+    request.cookies.get('next-auth.session-token')?.value ||
     request.cookies.get('__Secure-next-auth.session-token')?.value;
 
   if (!token && !isPublicPath && !isApiRoute) {
